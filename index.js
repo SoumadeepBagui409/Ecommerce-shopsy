6e50aeb776f9dd1c117cb387fb361fce6260d4c1
SoumadeepBagui409 <soumadeepbagui2@gmail.com>
2021-11-12 11:28:41 +0530

    changes in port

--- index.js
+++ index.js
@@ -82,4 +82,8 @@ app.use(authRoutes);
 
 
 var port = process.env.PORT || 3000; 
-app.listen(port);
+app.listen(port, ()=>{
+    console.log("Express server listen on port %d in %s mode",
+    this.address().port,
+    app.settings.env);
+});
